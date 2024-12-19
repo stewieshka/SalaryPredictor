b382681e6b23e511e47ec930661442a2e6c9a4af
Stepan <jojoba27.jojoba27@gmail.com>
2024-12-19 20:11:04 +0200

    feat: add predictor class

---
+++ src/SalaryPredictor.Bot/Predictor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.ML;
+using SalaryPredictor.Shared.Models;
+using SalaryPredictor.Training.Models;
+
+namespace SalaryPredictor.Bot;
+
+public static class Predictor
+{
+    private const string ModelPath = "SalaryPredictionModel.zip";
+    private static readonly MLContext MlContext = new();
+    private static readonly PredictionEngine<SalaryData,SalaryPrediction> PredictionEngine;
+
+    static Predictor()
+    {
+        var model = MlContext.Model.Load(ModelPath, out var modelInputSchema);
+        PredictionEngine = MlContext.Model.CreatePredictionEngine<SalaryData, SalaryPrediction>(model);
+    }
+
+    public static async Task<SalaryPrediction> PredictAsync(SalaryData salaryData)
+    {
+        return await Task.Run(() => PredictionEngine.Predict(salaryData));
+    }
+}
